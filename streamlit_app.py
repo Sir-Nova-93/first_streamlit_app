5ad7a174abbe97a6fef2e39c1277c0dbf8c77dba
Sir-Nova-93 <84126683+Sir-Nova-93@users.noreply.github.com>
2023-01-26 11:53:56 +0000

    Update streamlit_app.py
    Amended typo in get_fruityvice_data function parameter

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # Create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_coice):
+def get_fruityvice_data(this_fruit_choice):
 	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
 	# Take the json version of the response and normalize it
 	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
